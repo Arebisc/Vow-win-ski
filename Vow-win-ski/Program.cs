555ef6111f81c104887e97608e3b77ea75f2e681
Arebis <michal.sibera@windowslive.com>
2016-10-09 01:53:57 +0200

    Add project files.

---
+++ Vow-win-ski/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vow_win_ski
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
