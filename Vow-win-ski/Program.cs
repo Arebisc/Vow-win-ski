774fc433e78e642a8b288d6dfba5f7bf388df5cf
Dawid Wojciechowski <wojciechdavid@gmail.com>
2016-10-31 20:32:38 +0100

    Revert "29.10"
    This reverts commit 6cdc19bdeed2855b422fa374d8013f31ac3c6fd4.

--- Vow-win-ski/Program.cs
+++ Vow-win-ski/Program.cs
@@ -3,22 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading;
-using Vow_win_ski.IPC;
-
 
 namespace Vow_win_ski
 {
     class Program
     {
-
-
-
         static void Main(string[] args)
         {
-            
-
-
         }
     }
 }
