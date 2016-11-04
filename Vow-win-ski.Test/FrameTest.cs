3227afa60e0596f3edee76f9abf8beecd1bb45d0
dawidkulinski <dawid.kulinski@outlook.com>
2016-11-05 00:04:31 +0100

    Implementacja ramki

---
+++ Vow-win-ski.Test/FrameTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vow_win_ski.Memory;
+
+namespace Vow_win_ski.Test
+{
+    [TestClass]
+    public class FrameTest
+    {
+        [TestMethod]
+        public void CanWriteandReadData()
+        {
+            //przygotowanie
+            var frame1 = new Frame(1);
+            byte[] data = {0x22, 0x33, 0xaa, 0x76};
+
+            //dzialnie
+            frame1.WriteFrame(data);
+
+            //Assercje
+            Assert.AreEqual(frame1.ReadFrame().Length,4);
+            Assert.AreEqual(frame1.ReadFrame()[2],0xaa);
+        }
+
+        [TestMethod]
+        public void CanClearData()
+        {
+            var frame1 = new Frame(4);
+            byte[] data = {1, 2, 0xaa, 45};
+
+            frame1.WriteFrame(data);
+            frame1.ClearFrame();
+
+            Assert.AreEqual(frame1.ReadFrame()[10],0);
+        }
+    }
+}
---
+++ Vow-win-ski/Memory/Frame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Vow_win_ski.Memory
+{
+    public class Frame
+    {
+        private byte[] _fields;
+        public int Offset;
+        public bool IsValid;
+        public int FrameNumber;
+
+        public Frame(int number)
+        {
+            _fields = new byte[16];
+            ClearFrame();
+            Offset = 0;
+            IsValid = false;
+            FrameNumber = number;
+        }
+        public void WriteFrame(byte[] data)
+        {
+            Offset = data.Length;
+            IsValid = true;
+            for (int i = 0; i < data.Length; i++)
+            {
+                _fields[i] = data[i];
+            }
+        }
+        public byte[] ReadFrame()
+        {
+            return _fields.Take(Offset).ToArray();
+        }
+        public void ClearFrame()
+        {
+            Offset = 16;
+            IsValid = false;
+            for (int i = 0; i < _fields.Length; i++)
+            {
+                _fields[i] = 0x00;
+            }
+        }
+
+        public void ShowFrame()
+        {
+            foreach (var field in _fields)
+            {
+                Console.WriteLine(field);
+            }
+        }
+    }
+}
