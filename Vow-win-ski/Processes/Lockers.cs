f8008fd060f308cfac3947901e4d068d20532dca
Dannylach <dannylach@wp.pl>
2016-11-15 17:16:08 +0100

    Added lockers

---
+++ Vow-win-ski/Processes/Lockers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vow_win_ski.Processes
+{
+    class Lockers
+    {
+        byte open = 0;
+        Queue<Vow_win_ski.Processes.PCB> waiting;
+        int id;
+        PCB proces;
+
+        public void Lock()
+        {
+            if (open == 0)
+            {
+                this.id = proces.PID;
+                open = 1;
+            }
+            else
+            {
+                waiting.Enqueue(proces);
+                proces.State = Vow_win_ski.Processes.ProcessState.Waiting;
+            }
+        }
+
+        public void Unlock()
+        {
+            if (waiting.Count == 0)
+                open = 0;
+            else
+            {
+                proces = waiting.Dequeue();
+                proces.State = Vow_win_ski.Processes.ProcessState.Ready;
+            }
+        }
+    }
+}
