960c6247be05f5c742a849907abc15f250efe675
Dannylach <dannylach@wp.pl>
2016-11-29 15:15:09 +0100

    Chech()

--- Vow-win-ski/Processes/Lockers.cs
+++ Vow-win-ski/Processes/Lockers.cs
@@ -13,10 +13,15 @@ namespace Vow_win_ski.Processes
         int id;
         PCB proces;
 
-        public void Lock()
+        Lockers()
+        {
+        }
+
+        public void Lock(PCB Proces)
         {
             if (open == 0)
             {
+                proces = Proces;
                 this.id = proces.PID;
                 open = 1;
             }
@@ -27,15 +32,18 @@ namespace Vow_win_ski.Processes
             }
         }
 
-        public void Unlock()
+        public void Unlock(PCB Proces)
         {
-            if (waiting.Count == 0)
-                open = 0;
-            else
+            if (this.id == Proces.PID)
             {
                 proces = waiting.Dequeue();
                 proces.State = Vow_win_ski.Processes.ProcessState.Ready;
             }
+        }
+
+        public void Check()
+        {
+
         }
     }
 }
