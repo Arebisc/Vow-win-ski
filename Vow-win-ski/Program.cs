b003ef771962807be5d5f1dfca291a696855dc5c
Dawid Wojciechowski <wojciechdavid@gmail.com>
2016-11-29 16:15:56 +0100

    fixed program.cs

--- Vow-win-ski/Program.cs
+++ Vow-win-ski/Program.cs
@@ -12,27 +12,6 @@ namespace Vow_win_ski
         static void Main(string[] args)
         {
 
-            PipeServer server = new PipeServer();
-
-            PipeClient client = new PipeClient(1);
-
-            client.Connect();
-
-            client._send(2, 10);
-
-            client.Disconnect();
-
-            PipeClient client2 = new PipeClient(2);
-
-            client2.Connect();
-
-            client2._receive();
-
-            client2.Disconnect();
-
-
-
-
         }
     }
 }
