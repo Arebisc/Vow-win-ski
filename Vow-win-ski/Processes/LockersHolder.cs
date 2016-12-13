758e4879a188fe8121c59885a1b0d6d1c6b42fed
Dannylach <dannylach@wp.pl>
2016-12-13 16:32:41 +0100

    LockersHolder fixed

---
+++ Vow-win-ski/Processes/LockersHolder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vow_win_ski.Processes
+{
+    public sealed class LockersHolder
+    {
+        private static volatile LockersHolder _instance;
+        private static readonly object SyncRoot = new object();
+        public static Lockers MemoLock;
+        public static Lockers ProcLock;
+
+        public static void InitLockers()
+        {
+            _instance = new LockersHolder();
+        }
+
+        private LockersHolder()
+        {
+            Lockers MemoLock = new Lockers();
+            Lockers ProcLock = new Lockers();
+        }
+
+        public static LockersHolder GetInstance => _instance;
+    }
+}
