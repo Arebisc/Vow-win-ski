35901d6403df88463e5ff9121727ec2e806be9cd
Dannylach <dannylach@wp.pl>
2017-01-12 14:05:24 +0100

    Fixed jump action

--- Vow-win-ski/CPU/Interpreter.cs
+++ Vow-win-ski/CPU/Interpreter.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -264,9 +263,9 @@ namespace Vow_win_ski.CPU
         {
             Console.WriteLine("Rozkaz JM z parametrem " + tag);
 
+            CPU.GetInstance.Register.C--;
             if (CPU.GetInstance.Register.C != 0)
             {
-                CPU.GetInstance.Register.C--;
                 var runningPCB = Scheduler.GetInstance.GetRunningPCB();
                 runningPCB.InstructionCounter = 0;
 
