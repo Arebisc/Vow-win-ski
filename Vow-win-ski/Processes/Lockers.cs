f7ee9b352ca73cbf561136a064ea74f7c244c18d
Dannylach <dannylach@wp.pl>
2017-01-19 23:09:03 +0100

    Locki

--- Vow-win-ski/Processes/Lockers.cs
+++ Vow-win-ski/Processes/Lockers.cs
@@ -43,9 +43,9 @@ namespace Vow_win_ski.Processes
             {
                 if (waiting.Count() > 0)
                 {
-                    if (Check(name))
+                    if (!Check(name))
                     {
-                        foreach(var i in waiting)
+                        foreach (var i in waiting)
                         {
                             if (name == i.Name)
                             {
@@ -54,6 +54,13 @@ namespace Vow_win_ski.Processes
                                 break;
                             }
                         }
+                    }
+                    else
+                    {
+                        proces.StopWaiting();
+                        proces = waiting[0];
+                        waiting.Remove(proces);
+                        this.Name = proces.Name;
                     }
                 }
                 else if (waiting.Count() == 0)
